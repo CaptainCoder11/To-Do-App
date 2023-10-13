b84fdb9b469e9b37ff3b37ca34d9986d54053bbe
Ruchit Patel <ruchitpatel@MacBook-Air.local>
2023-10-13 18:22:00 +0530

    Changes browserrouter to hash router

--- src/components/shared/MeetingCard.jsx
+++ src/components/shared/MeetingCard.jsx
@@ -1,5 +1,7 @@
-function MeetingCard({time,title,userlist = [""],bgcol}){
-    return (<div className={`w-auto mr-7 rounded-2xl mt-10 md:mt-3 h-44 bg-meetingbg${bgcol}`}>
+function MeetingCard({time,title,userlist = [""],bgcol=1}){
+    let bg = 'bg-meetingbg' + bgcol
+    console.log(bg)
+    return (<div className={`w-auto mr-7 rounded-2xl mt-10 md:mt-3 h-44 ${bg}`}>
     <h1 className=" font-Sofia-Pro-Ultra-Light md:text-xs text-meetingtitle pt-9 md:pt-7 ml-7 md:ml-4">{time}</h1>
     <h1 className=" font-Sofia-Pro-Light md:text-xs text-meetingtitle mt-4 ml-7 md:ml-4">{title}</h1>
     <div className="flex flex-row mt-4 md:mt-7 ml-7 md:ml-4">
